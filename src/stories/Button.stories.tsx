0058ba0a8b6a2f7be92b441275cbcd341dedeab9
npallav <nav.pallav@mercedes-benz.com>
2023-08-25 17:07:34 +0530

    With click functionality added, stable verion of 0.0.6, getting produced!

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { StoryFn, Meta } from "@storybook/react";
-
-import Button from "../components/Button/Button"; // Replace with your actual component path
+import Button from "../components/Button/Button";
 
 export default {
   title: "Example/Button",
@@ -13,9 +12,15 @@ const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   label: "Storybook Primary Button",
+  onClick: () => {
+    console.log("Primary button clicked!");
+  },
 };
 
 export const Secondry = Template.bind({});
 Secondry.args = {
   label: "Storybook Secondry Button",
+  onClick: () => {
+    console.log("Secondry button clicked!");
+  },
 };
